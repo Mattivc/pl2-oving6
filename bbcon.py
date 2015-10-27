00d262afad67c001fd80aca76e58cdc8c237c0ec
maaland <mariusmaaland@gmail.com>
2015-10-27 11:41:38 +0100

    Add set_arbit method, update sensors, invoke arbit and update motobs

--- bbcon.py
+++ bbcon.py
@@ -11,7 +11,7 @@ class Bbcon(object):
         self.active_behavs = []     # Active behaviours
         self.sensobs = []           # Sensory objects
         self.motobs = []            # Motor objects
-        self.arbit = None           # Arbitrator
+        self.arbit = None          # Arbitrator
         self.time_step = time_step
 
         def add_behaviour(self, behaviour):
@@ -34,14 +34,24 @@ class Bbcon(object):
             else:
                 return True
 
+        def set_arbit(self, arbitrator):
+            self.arbit = arbitrator
+
         def run_one_timestep(self):
             # Update sensors
-
+            for sensob in self.sensobs:
+                sensob.update()
             # Update behaviours
 
             # Invoke arbitrator
+            motor_rec = self.arbit.choose_action()     #returns a tuple of (motor_recommendation, halt)
+
 
             # Update motobs
+            i = 0
+            for motob in self.motobs:     #updates each motob with it's respective motor recommendation
+                motob.update(motor_rec(0)[i])
+                i = i +1
 
             # Wait
 
