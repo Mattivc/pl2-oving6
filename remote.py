cdba67f9a86a3a506cd9b8b928c23956e2c7bf7e
Matias Christensen <mattivc@gmail.com>
2015-10-27 11:58:47 +0100

    updated remote

--- remote.py
+++ remote.py
@@ -3,18 +3,20 @@ from time import sleep
 
 mot = motors.Motors()
 
-duration = 0.25
-speed = 1.0
+duration = 0.2
+speed = 0.5
 
 while True:
 
     move = input('Input: ')
-    if move == 'w':
-        mot.forward(speed, duration)
-    elif move == 's':
-        mot.backward(speed, duration)
-    elif move == 'a':
-        mot.left(speed, duration)
-    elif move == 'd':
-        mot.right(speed, duration)
-    sleep(duration)
+
+    for c in move:
+        if c == 'w':
+            mot.forward(speed, duration)
+        elif c == 's':
+            mot.backward(speed, duration)
+        elif c == 'a':
+            mot.left(speed, duration)
+        elif c == 'd':
+            mot.right(speed, duration)
+        sleep(duration*1.2)
