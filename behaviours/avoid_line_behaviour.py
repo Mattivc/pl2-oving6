f449d18ab4564c8af3249180b9f05e36ac5139d7
Kristian Rekstad <krirek@msn.com>
2015-10-29 14:52:18 +0100

    Fleshed out avoid_line_behaviour. Made helper method in motob for motors motob.

--- behaviours/avoid_line_behaviour.py
+++ behaviours/avoid_line_behaviour.py
@@ -13,5 +13,25 @@ class AvoidLineBehaviour(behav.Behaviour):
         """
         if not isinstance(line_sensob, LineSensob):
             raise Exception("Invalid type for line_sensob: "+type(line_sensob))
+        super().__init__(bbcon, [line_sensob])
 
-        super().__init__(bbcon, list(line_sensob))
+        self.PRIORITY = 50
+
+    def consider_deactivation(self):
+        self.active_flag = True
+
+    def consider_activation(self):
+        self.active_flag = True
+
+    def sense_and_act(self):
+        line_sensob = self.sensobs[0]
+        if not isinstance(line_sensob, LineSensob):
+            raise Exception("Invalid type for line_sensob: "+type(line_sensob))
+
+        found_lines = line_sensob.get_found_lines()
+        position = line_sensob.get_line_position()
+
+        self.set_match_degree(1.0 if found_lines else 0.0)
+
+
+
