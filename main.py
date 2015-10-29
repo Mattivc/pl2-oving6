83ea989f08ffe2e70c9df102e10ef6bbc7211a11
Kristian Rekstad <krirek@msn.com>
2015-10-29 17:35:10 +0100

    Added following of red objects

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from bbcon import Bbcon
 from behaviours.avoid_line_behaviour import AvoidLineBehaviour
+from behaviours.follow_red_behaviour import FollowRedBehaviour
 from sensobs.line_sensob import LineSensob
+from sensobs.red_detector import RedDetector
 from sensors import *
 from robot.motors import Motors
 from motob import WheelMotob
@@ -24,6 +26,16 @@ if __name__ == '__main__':
     wheel_motob = WheelMotob(motors, bb.time_step)
     bb.motobs.append(wheel_motob)
 
+
+    # Camera
+
+    camera_sensor = cam.Camera(img_rot=180)
+    follow_red_sensob = RedDetector(camera_sensor)
+    follow_red_behaviour = FollowRedBehaviour(bb, follow_red_sensob)
+    bb.add_sensor(camera_sensor)
+    bb.add_sensory_object(follow_red_sensob)
+    bb.add_behaviour(follow_red_behaviour)
+    bb.activate_behaviour(follow_red_behaviour)
 
     try:
         while bb.run_one_timestep():
